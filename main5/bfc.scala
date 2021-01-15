be5452e682db3a92c7fc364d779fa2b4633bab28
Nishaanth Elango <nishaanthelango@gmail.com>
2021-01-15 19:16:03 +0800

    moved bf functions to bfc

--- main5/bfc.scala
+++ main5/bfc.scala
@@ -34,6 +34,64 @@ type Mem = Map[Int, Int]
 import io.Source
 import scala.util._
 
+def load_bff(name: String) : String = {
+    Try(Source.fromFile(name).mkString).getOrElse("")
+}
+
+def sread(mem: Mem, mp: Int) : Int = {
+    mem.getOrElse(mp,0)
+}
+
+def write(mem: Mem, mp: Int, v: Int) : Mem = {
+    mem.updated(mp, v)
+}
+
+def jumpRight(prog: String, pc: Int, level: Int) : Int = {
+
+    if( (pc > prog.length - 2) || (prog(pc) == ']' && level == 0))pc+1
+    else if (level != 0 && prog(pc) == ']') jumpRight(prog, pc + 1, level - 1)
+    else if (prog(pc) == '[') jumpRight(prog, pc + 1, level + 1)
+    else jumpRight(prog, pc + 1, level)
+
+}
+
+def jumpLeft(prog: String, pc: Int, level: Int) : Int = {
+    
+    if(level == 0 && prog(pc) == '[') pc + 1
+    else if(level != 0 && pc == 0) - 1
+    else if(level != 0 && prog(pc) == '[') jumpLeft(prog, pc - 1, level - 1)
+    else if(prog(pc) == ']') jumpLeft(prog, pc - 1, level + 1)
+    else jumpLeft(prog, pc - 1, level)
+
+}
+
+def compute(prog: String, pc: Int, mp: Int, mem: Mem) : Mem = {
+    if (pc > prog.length() - 1) mem
+    else prog.charAt(pc) match {
+        case '>' => compute(prog, pc + 1, mp + 1, mem)
+        case '<' => compute(prog, pc + 1, mp - 1, mem)
+        case '+' => compute(prog, pc + 1, mp, write(mem, mp, sread(mem, mp) + 1))
+        case '-' => compute(prog, pc + 1, mp, write(mem, mp, sread(mem, mp) - 1))
+        case '.' => print(sread(mem, mp).toChar); compute(prog, pc + 1, mp, mem)
+        case ',' => compute(prog, pc + 1, mp, mem + (mp -> Console.in.read().toByte))
+        case '[' => {
+            if (sread(mem, mp) == 0) compute(prog, jumpRight(prog, pc + 1, 0), mp, mem) 
+            else compute(prog, pc + 1, mp, mem)
+        }
+        case ']' => {
+            if (sread(mem, mp) != 0) compute(prog, jumpLeft(prog, pc - 1, 0), mp, mem) 
+            else compute(prog, pc + 1, mp, mem)
+        }
+        case '*' => compute(prog, pc + 1, mp, write(mem, mp, sread(mem, mp) * sread(mem,mp-1)))
+        case '@' => compute(prog, pc + 1, mp, write(mem, mem(mp), sread(mem,mp-1) ))
+        case '#' => print(sread(mem, mp)); compute(prog, pc + 1, mp, mem)
+        case _ => compute(prog, pc + 1, mp, mem)
+  }
+}
+
+def run(prog: String, m: Mem = Map()) = {
+    compute(prog,0,0,m)
+}
 
 // TASKS
 //=======
